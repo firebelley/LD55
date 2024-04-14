71f9842f1a5aafde3be214f43cf0bd798da5bc9f
Firebelley <firebelley@users.noreply.github.com>
2024-04-14 17:13:35 -0400

    fix settings stuff

--- scenes/autoload/settings.gd
+++ scenes/autoload/settings.gd
@@ -1,7 +1,16 @@
 extends Node
 
 
+func _ready():
+	if (!OS.has_feature("web") && !OS.is_debug_build()):
+		get_window().mode = Window.MODE_FULLSCREEN
+		get_window().borderless = true
+
+
 func _unhandled_input(event):
+	if (OS.has_feature("web")):
+		return
+
 	if (event.is_action_pressed("fullscreen")):
 		if get_window().mode == Window.MODE_FULLSCREEN:
 			get_window().mode = Window.MODE_WINDOWED
@@ -14,4 +23,4 @@ func _unhandled_input(event):
 		var bus_idx = AudioServer.get_bus_index("Master")
 		var is_muted = AudioServer.is_bus_mute(bus_idx)
 		AudioServer.set_bus_mute(bus_idx, !is_muted)
-		
+
